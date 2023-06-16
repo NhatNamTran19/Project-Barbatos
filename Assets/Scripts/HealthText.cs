96ba74c512d61125a550782b0229329ff057c596
NHATNAM\NAM <himalaya103@gmail.com>
2023-06-17 05:52:45 +0700

    [add] Health Text Damage

---
+++ Assets/Scripts/HealthText.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;using TMPro;
+
+public class HealthText : MonoBehaviour
+{
+    [SerializeField] private Vector3 moveSpeed = new Vector3(0,75,0);
+    [SerializeField] private float timeToFade = 2f;
+
+    private float timeElapsed=0f;
+    private Color startColor;
+
+
+    RectTransform textTranform;
+    TextMeshProUGUI textMeshPro;
+    void Start()
+    {
+        textTranform = GetComponent<RectTransform>();
+        textMeshPro = GetComponent<TextMeshProUGUI>();
+        startColor = textMeshPro.color;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        textTranform.position += moveSpeed * Time.deltaTime;
+        timeElapsed += Time.deltaTime;
+
+        if(timeElapsed < timeToFade)
+        {
+            float fadeAlpha = startColor.a * (1 - (timeElapsed / timeToFade));
+            textMeshPro.color = new Color(startColor.r,startColor.g,startColor.b, fadeAlpha);          
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+}
